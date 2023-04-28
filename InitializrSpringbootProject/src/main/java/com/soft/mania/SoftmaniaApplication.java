36f26874c03238ebf1f56e77f126f660daa8d815
Marcola <51445096+Marcos-kaneck@users.noreply.github.com>
2023-04-28 17:18:00 -0400

    Add files via upload

---
+++ InitializrSpringbootProject/src/main/java/com/soft/mania/SoftmaniaApplication.java
@@ -0,0 +1,13 @@
+package com.soft.mania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SoftmaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmaniaApplication.class, args);
+	}
+}
---
+++ InitializrSpringbootProject/src/test/java/com/soft/mania/SoftmaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.mania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
